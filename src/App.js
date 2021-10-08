cb019990168bc473f869a629afa99d8726ce90e2
Davin anaya <davin.anaya.s@gmail.com>
2021-10-08 17:39:12 -0500

    add styles and validations for items

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
+import dataItems from "./items.json";
+
 import './App.css';
 
 function App() {
   const [input, setInput] = useState('');
-  const [tags, setTags] = useState([]);
+  const [items, setItems] = useState([]);
+  const [showAlert, setShowAlert] = useState(false);
 
   const onChange = (e) => {
     const { value } = e.target;
@@ -12,33 +15,62 @@ function App() {
   
   const onKeyDown = (e) => {
     const { key } = e;
-    const trimmedInput = input.trim();
-  
-    if (key === 'Enter' && trimmedInput.length && !tags.includes(trimmedInput)) {
+    
+    if (key === 'Enter') {
+      const trimmedInput = input.trim().toLowerCase();
+      const itemIndex = items.findIndex(({ productName }) => productName === trimmedInput);
+      const itemExist = dataItems.items.find(({ name }) => name.toLowerCase() === trimmedInput);
+      if (!itemExist) {
+        setShowAlert(true);
+        return;
+      }
+
       e.preventDefault();
-      setTags(prevState => [...prevState, trimmedInput]);
+
+      if (itemIndex >= 0) {
+        items[itemIndex].qty += 1;
+        setItems(items);
+      } else {
+        const newProduct = { productName: trimmedInput, qty: 1};
+        setItems(prevState => [...prevState, newProduct]);
+      }
+
       setInput('');
     }
   };
 
   const deleteTag = (index) => {
-    setTags(prevState => prevState.filter((tag, i) => i !== index))
+    setItems(prevState => prevState.filter((tag, i) => i !== index))
   }
-  
   return (
     <div className="container">
-      {tags.map((tag, index) => (
-        <div className="tag" key={index}>
-          {tag}
-          <button onClick={() => deleteTag(index)}>x</button>
-        </div>
-      ))}
-      <input
-        value={input}
-        placeholder="Enter a product"
-        onKeyDown={onKeyDown}
-        onChange={onChange}
-      />
+      <div className="row">
+        <input
+          value={input}
+          className="form-control "
+          placeholder="Enter a product"
+          onKeyDown={onKeyDown}
+          onChange={onChange}
+        />
+        {showAlert ? (<div className="alert alert-danger mt-2" role="alert">
+          This item is not map in our system, please check the <strong>items.json</strong> file
+          <button type="button" class="close" onClick={() => setShowAlert(!showAlert)}>
+              <span aria-hidden="true">&times;</span>
+          </button>
+        </div>) :  null}
+      </div>
+      <div className="row mt-2 mb-2">
+          {items.map(({ qty, productName}, index) => (
+            <div className="tag" key={index}>
+              {`${qty} ${productName}`}
+              <button onClick={() => deleteTag(index)}>x</button>
+            </div>
+          ))}
+      </div>
+      {items.length ? (<div className="row">
+        <button type="button" className="btn btn-dark">Total</button>
+      </div>): null}
+      
   </div>
   );
 }
